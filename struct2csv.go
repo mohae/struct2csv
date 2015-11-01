605b56f1aa74f195debdbf430199c03b85a4ad74
Joel Scoble <joel.scoble@outlook.com>
2015-11-01 22:35:25 +0000

    add transcoder struct, add support for tag configuration

--- struct2csv.go
+++ struct2csv.go
@@ -2,9 +2,38 @@
 package struct2csv
 
 import (
-  "fmt"
-  "reflect"
+	"fmt"
+	"reflect"
 )
+
+// Transcoder handles transcoding from a struct to CSV
+type Transcoder struct {
+	// Whether or not tags should be use for header (column) names; by default this is csv,
+	useTags bool
+	tag     string // The tag to use when tags are being used for headers; defaults to csv.
+	csv     [][]byte
+}
+
+// NewTranscoder returns an initialized transcoder
+func NewTranscoder() *Transcoder {
+	return &Transcoder{useTags: true, tag: "csv"}
+}
+
+// SetUseTags sets whether or not tags should be used for header (column)
+// names.
+func (t *Transcoder) SetUseTags(b bool) {
+	t.useTags = b
+}
+
+// SetTag sets the tag that the Transcoder should use for header (column)
+// names.  By default, this is set to 'csv'.  If the received value is an
+// empty string, nothing will be done
+func (t *Transcoder) SetTag(s string) {
+	if s == "" {
+		return
+	}
+	t.tag = s
+}
 
 // GetHeaders get's the column headers from the received struct.  If anything
 // other than a struct is passed, an error will be returned.  If the struct
@@ -13,24 +42,39 @@ import (
 //
 // If field tags other than the ones for csv are to be used, TODO figure out
 // the struct and how to implement this comment.
+func (t *Transcoder) GetHeaders(v interface{}) ([]string, error) {
+	if reflect.TypeOf(v).Kind() != reflect.Struct {
+		return nil, fmt.Errorf("struct required: value was of type %s", reflect.TypeOf(v).Kind())
+	}
+	return t.getHeaders(v)
+}
+
+func (t *Transcoder) getHeaders(v interface{}) ([]string, error) {
+	s := reflect.TypeOf(v)
+	var hdrs []string
+	for i := 0; i < s.NumField(); i++ {
+		f := s.Field(i)
+		// skip unexported
+		if f.PkgPath != "" {
+			continue
+		}
+		var name string
+		if t.useTags {
+			name = f.Tag.Get(t.tag)
+		}
+		// If there isn't a matching field tag, use the Field Name
+		if name == "" {
+			name = f.Name
+		}
+		hdrs = append(hdrs, name)
+	}
+	return hdrs, nil
+}
+
+// GetHeaders instantiates a Transcoder and gets the headers of the received
+// struct.  If you need more control over tag processing, use NewTranscoder(),
+// set accordingly, and call Transcoder's GetHeaders().
 func GetHeaders(v interface{}) ([]string, error) {
-  if reflect.TypeOf(v).Kind() != reflect.Struct {
-    return nil, fmt.Errorf("struct required: value was of type %s", reflect.TypeOf(v).Kind())
-  }
-  s := reflect.TypeOf(v)
-  var hdrs []string
-  for i := 0; i < s.NumField(); i++ {
-    f := s.Field(i)
-    // skip unexported
-    if f.PkgPath != "" {
-      continue
-    }
-    name := f.Tag.Get("csv")
-    // If there isn't a matching field tag, use the Field Name
-    if name == "" {
-      name = f.Name
-    }
-    hdrs = append(hdrs, name)
-  }
-  return hdrs, nil
+	tc := NewTranscoder()
+	return tc.GetHeaders(v)
 }
--- struct2csv_test.go
+++ struct2csv_test.go
@@ -7,30 +7,55 @@ import (
 	_ "github.com/mohae/customjson"
 )
 
-type TstTags struct {
-	Int     int      `json:"number" csv:"Number"`
-	Ints    []int    `json:"numbers" csv:"Numbers"`
-  ints    []int
-	String  string   `json:"word" csv:"Word"`
-	Strings []string `json:"words" csv:"Words"`
-  StringString  map[string]string `json:"mapstringstring" csv:"MapStringString"`
-  StringInt  map[string]int `json:"mapstringInt" csv:"MapStringInt"`
-  IntInt     map[int]int `json:"mapintint" csv:"MapIntInt"`
-  strings []string
-}
-
 type Tst struct {
-	Int     int
-	Ints    []int
-  ints    []int
-	String  string
-	Strings []string
-  strings []string
-  StringString  map[string]string
-  StringInt  map[string]int
-  IntInt     map[int]int
+	Int          int
+	Ints         []int
+	ints         []int
+	String       string
+	Strings      []string
+	StringString map[string]string
+	StringInt    map[string]int
+	IntInt       map[int]int
+	strings      []string
 }
 
+type TstTags struct {
+	Int          int   `json:"number" csv:"Number"`
+	Ints         []int `json:"numbers" csv:"Numbers"`
+	ints         []int
+	String       string            `json:"word" csv:"Word"`
+	Strings      []string          `json:"words" csv:"Words"`
+	StringString map[string]string `json:"mapstringstring" csv:"MapStringString"`
+	StringInt    map[string]int    `json:"mapstringint" csv:"MapStringInt"`
+	IntInt       map[int]int       `json:"mapintint" csv:"MapIntInt"`
+	strings      []string
+}
+
+func TestNewTranscoder(t *testing.T) {
+	tc := NewTranscoder()
+	if tc.useTags != true {
+		t.Errorf("expected useTags to be true got %t", tc.useTags)
+	}
+	if tc.tag != "csv" {
+		t.Errorf("expected transcoder's tag to be \"csv\", got %q", tc.tag)
+	}
+	tc.SetUseTags(false)
+	if tc.useTags != false {
+		t.Errorf("expected useTags to be false got %t", tc.useTags)
+	}
+	tc.SetTag("json")
+	if tc.tag != "json" {
+		t.Errorf("expected transcoder's tag to be \"json\", got %q", tc.tag)
+	}
+	tc.SetUseTags(true)
+	if tc.useTags != true {
+		t.Errorf("expected useTags to be false got %t", tc.useTags)
+	}
+	tc.SetTag("csv")
+	if tc.tag != "csv" {
+		t.Errorf("expected transcoder's tag to be \"csv\", got %q", tc.tag)
+	}
+}
 func TestGetHeaders(t *testing.T) {
 	_, err := GetHeaders([]string{"a", "b", "c"})
 	if err == nil {
@@ -40,38 +65,100 @@ func TestGetHeaders(t *testing.T) {
 			t.Errorf("expected error to be \"struct required: value was of type slice\", got %q", err)
 		}
 	}
-	test := Tst{}
+	tc := NewTranscoder()
+	tc.useTags = false
+	tst := Tst{}
 	expectedHeaders := []string{"Int", "Ints", "String", "Strings", "StringString", "StringInt", "IntInt"}
-	hdr, err := GetHeaders(test)
+	hdr, err := tc.GetHeaders(tst)
 	if err != nil {
 		t.Errorf("unexpected error getting header information from Tst{}: %q", err)
-    goto TAG
+		goto IGNORETAG
 	}
-  if len(hdr) != len(expectedHeaders) {
+	if len(hdr) != len(expectedHeaders) {
 		t.Errorf("Expected %d column headers, got %d", len(expectedHeaders), len(hdr))
-		goto TAG
+		goto IGNORETAG
 	}
 	for i, v := range hdr {
 		if v != expectedHeaders[i] {
-			t.Errorf("%d: expected %q got %q", expectedHeaders[i], v)
+			t.Errorf("%d: expected %q got %q", i, expectedHeaders[i], v)
 		}
 	}
 
-TAG:
-   // test using CSV tags
-   expectedHeaders = []string{"Number", "Numbers", "Word", "Words", "MapStringString", "MapStringInt", "MapIntInt"}
-   hdr, err = GetHeaders(TstTags{})
- 	if err != nil {
- 		t.Errorf("unexpected error getting header information from Tst{}: %q", err)
-     return
- 	}
-   if len(hdr) != len(expectedHeaders) {
- 		t.Errorf("Expected %d column headers, got %d", len(expectedHeaders), len(hdr))
- 		return
- 	}
- 	for i, v := range hdr {
- 		if v != expectedHeaders[i] {
- 			t.Errorf("%d: expected %q got %q", expectedHeaders[i], v)
- 		}
- 	}
+IGNORETAG:
+	tc.useTags = false
+	test := TstTags{}
+	expectedHeaders = []string{"Int", "Ints", "String", "Strings", "StringString", "StringInt", "IntInt"}
+	hdr, err = tc.GetHeaders(test)
+	if err != nil {
+		t.Errorf("unexpected error getting header information from Tst{}: %q", err)
+		goto CSVTAG
+	}
+	if len(hdr) != len(expectedHeaders) {
+		t.Errorf("Expected %d column headers, got %d", len(expectedHeaders), len(hdr))
+		goto CSVTAG
+	}
+	for i, v := range hdr {
+		if v != expectedHeaders[i] {
+			t.Errorf("%d: expected %q got %q", i, expectedHeaders[i], v)
+		}
+	}
+
+CSVTAG:
+	// test using CSV tags
+	tc.useTags = true
+	expectedHeaders = []string{"Number", "Numbers", "Word", "Words", "MapStringString", "MapStringInt", "MapIntInt"}
+	hdr, err = tc.GetHeaders(TstTags{})
+	if err != nil {
+		t.Errorf("unexpected error getting header information from Tst{}: %q", err)
+		goto JSONTAG
+	}
+	if len(hdr) != len(expectedHeaders) {
+		t.Errorf("Expected %d column headers, got %d", len(expectedHeaders), len(hdr))
+		goto JSONTAG
+	}
+	for i, v := range hdr {
+		if v != expectedHeaders[i] {
+			t.Errorf("%d: expected %q got %q", i, expectedHeaders[i], v)
+		}
+	}
+
+JSONTAG:
+	// test using CSV tags
+	expectedHeaders = []string{"number", "numbers", "word", "words", "mapstringstring", "mapstringint", "mapintint"}
+	tc.useTags = true
+	tc.tag = "json"
+	hdr, err = tc.GetHeaders(TstTags{})
+	if err != nil {
+		t.Errorf("unexpected error getting header information from Tst{}: %q", err)
+		goto JSONTAG
+	}
+	if len(hdr) != len(expectedHeaders) {
+		t.Errorf("Expected %d column headers, got %d", len(expectedHeaders), len(hdr))
+		goto JSONTAG
+	}
+	for i, v := range hdr {
+		if v != expectedHeaders[i] {
+			t.Errorf("%d: expected %q got %q", i, expectedHeaders[i], v)
+		}
+	}
+
+	//EMBED:
+	/*
+	    expectedHeaders = []string{"Name", "Ingredients", "Instructions", "ID", "Addr1", "Addr2", "City", "State", "Zip", "Phone", "Long", "Lat"}
+	    hdr, err = GetHeaders(TstEmbed{})
+	    if err != nil {
+	   		t.Errorf("unexpected error getting header information from Tst{}: %q", err)
+	       return
+	   	}
+	     if len(hdr) != len(expectedHeaders) {
+	   		t.Errorf("Expected %d column headers, got %d", len(expectedHeaders), len(hdr))
+	      t.Errorf("%#v\n", hdr)
+	   		return
+	    }
+	   	for i, v := range hdr {
+	   		if v != expectedHeaders[i] {
+	   			t.Errorf("%d: expected %q got %q", expectedHeaders[i], v)
+	   		}
+	   	}
+	*/
 }
